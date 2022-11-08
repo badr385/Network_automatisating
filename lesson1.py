ccc8456d35495223f82f8919844596cff725e56d
Badr <59146833+badr385@users.noreply.github.com>
2022-11-08 14:06:46 +0100

    Create lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+#The first lesson to understand python basics.
+
+Python is a cool language, even if it's executing one after one task, but also knowing how it s working u can using threading and execute ur code simmulatnously on it. Be patient we will abord this point when it s time.
+
+#How to set a variable on python, it s pretty easy and cool and u can stock ur info on it to call it on another part of a code
+
+my_variable = "hello world"
+
+#how to print a variable
+print(my_variable)
